83851dbdc78f03978ba0b7642027e7861613e6cb
Drew Keck <n01367733@unf.edu>
2025-01-30 16:36:44 -0500

    added some more basic logic and upgrade functionality. BUG -- only one automatic upgrade works at a time?

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //VARIABLES
-var speedCount = document.querySelector('.speedCount')
-var agilityCount = 0;
-var strengthCount = 0;
-var gritCount = 0;
-var intelligenceCount = 0;
-var skillCount = 0;
-var honorCount = 0;
+var _SPEEDCOUNT;
+var _AGILITYCOUNT;
+var _STRENGTHCOUNT;
+var _GRITCOUNT;
+var _INTELLIGENCECOUNT;
+var _SKILLCOUNT;
+var _HONORCOUNT;
+var _STORMLIGHTCOUNT
 
 
 var i = 0;
@@ -33,13 +34,68 @@ function progressClick(element, attribute) {
     }
 }
 
+function recursiveProgress(element, attribute) {
+    if (i == 0)
+        {
+            i = 1;
+            var elem = document.getElementById(element);
+            var width = 1;
+            var id = setInterval(frame, 10);
+            function frame()
+            {
+                if (width >= 100)
+                {
+                    clearInterval(id);
+                    i = 0;
+                    elem.style.width = 0 + '%'
+                    updateCount(attribute)
+                    recursiveProgress(element, attribute);
+                } else
+                {
+                    width++;
+                    elem.style.width = width + '%';
+                }
+            }
+        }
+}
+
 function updateCount(attr) {
     var elem = document.querySelector('.' + attr);
     elem.innerHTML = parseFloat(elem.innerHTML) + 1;
+    switch(attr)
+    {
+        case 'speedCount':
+            _SPEEDCOUNT = elem.innerHTML;
+            break;
+        case 'agilityCount':
+            _AGILITYCOUNT = elem.innerHTML;
+            break;
+        case 'strengthCount':
+            _STRENGTHCOUNT = elem.innerHTML;
+            break;
+        case 'gritCount':
+            _GRITCOUNT = elem.innerHTML;
+            break;
+        case 'intelligenceCount':
+            _INTELLIGENCECOUNT = elem.innerHTML;
+            break;
+        case 'skillCount':
+            _SKILLCOUNT = elem.innerHTML;
+            break;
+        case 'honorCount':
+            _HONORCOUNT = elem.innerHTML;
+            break;
+        case 'stormlightCount':
+            _STORMLIGHTCOUNT = elem.innerHTML;
+            break;
+    }
 
     if(attr == 'strengthCount' && elem.innerHTML == 5)
     {
-        window.confirm("soldier?");
+        if(window.confirm("You've been selected to become a soldier? Will you go? (this choice has consequences and cannot be changed."))
+        {
+            //replace buttons for soldier
+        } 
     }
     else if(attr == 'intelligenceCount' && elem.innerHTML == 5)
     {
@@ -50,6 +106,17 @@ function updateCount(attr) {
         window.alert("You've been chosen by a spren!");
         document.querySelector('.stormlightText').innerHTML = 'Stormlight:';
         document.getElementById('stormlightBar').style.display = '';
+    }
+}
+
+function upgrade(clicked) {
+    if(clicked == 'upgrade1')
+    {
+        recursiveProgress('speed-button', 'speedCount');
+    }
+    else if(clicked == 'upgrade2')
+    {
+        recursiveProgress('agility-button', 'agilityCount');
     }
 }
 
